5308dcace98b4291953a44bd3538ab76dc4b9d9d
chrisw-ibm <104161708+chrisw-ibm@users.noreply.github.com>
2024-09-30 09:41:04 -0400

    fix: #7 Fix Upgrade Test (#8)
    * fix: #7 Fix Upgrade Test
    
    Signed-off-by: Chris Waddington <104161708+chrisw-ibm@users.noreply.github.com>

--- modules/cloudability-bucket-access/outputs.tf
+++ modules/cloudability-bucket-access/outputs.tf
@@ -1,6 +1,6 @@
 output "custom_role_display_name" {
   description = "Display name of the cos custom role"
-  value       = ibm_iam_custom_role.cos_custom_role.display_name
+  value       = local.custom_role
 }
 
 output "service_policy" {
